bf8827e323ebe2a631985e1340a9336bdde96f84
wewineitor <gamerisaac48@gmail.com>
2021-11-20 16:24:01 -0600

    terminado

--- js/scripts/comprar.js
+++ js/scripts/comprar.js
@@ -2,27 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const boton = document.getElementById("comprar");
 
     boton.addEventListener('click', () => {
-        $.ajax({
-            type: 'post',
-            processData: false,
-            cache: false,
-            dataType: 'text',
-            contentType: "application/json; charset=UTF-8",
-            url: './php/scripts/comprar.php',
-            data: JSON.stringify({usuario: localStorage.getItem("usuario")}),
-            success: () => {
-                Swal.fire({
-                    icon: 'success',
-                    text: 'Compra realizada'
-                })
-                obtenerCarrito()
-            },
-            error: () => {
-                Swal.fire({
-                    icon: 'error',
-                    text: 'Error con la conexion'
-                })
-            }
-        })
+        if(localStorage.getItem("usuario") == null) {
+            Swal.fire({
+                icon: 'error',
+                title: 'ERROR',
+                text: 'Debe conectarse para poder realizar esta accion'
+            })
+        }
+        else {
+            $.ajax({
+                type: 'post',
+                processData: false,
+                cache: false,
+                dataType: 'text',
+                contentType: "application/json; charset=UTF-8",
+                url: './php/scripts/comprar.php',
+                data: JSON.stringify({usuario: localStorage.getItem("usuario")}),
+                success: () => {
+                    Swal.fire({
+                        icon: 'success',
+                        text: 'Compra realizada',
+                        confirmButtonText: 'Aceptar'
+                    }).then(result => {
+                        if(result.isConfirmed) location.reload()
+                    })
+                    
+                },
+                error: () => {
+                    Swal.fire({
+                        icon: 'error',
+                        text: 'Error con la conexion'
+                    })
+                }
+            })
+        }
     });
 });
